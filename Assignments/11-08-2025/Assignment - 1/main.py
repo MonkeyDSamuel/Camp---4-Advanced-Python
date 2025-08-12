6eb1602f289a80f81326b53de773831f3872918b
MonkeyDSamuel <samueleternityunofficial@gmail.com>
2025-08-12 13:01:11 +0530

    Assignment updated and completed assignment - 1 of 11-08-2025

---
+++ Assignments/11-08-2025/Assignment - 1/main.py
@@ -0,0 +1,5 @@
+from lib.menudriven import MenuDriven
+
+if __name__ == '__main__':
+    app = MenuDriven()
+    app.run()
---
+++ Assignments/11-08-2025/Assignment - 1/models/Interest.py
@@ -0,0 +1,74 @@
+'''
+1. 
+    A bank has two kinds of accounts, savings and fixed deposit. 
+    The interest rates for these are 4% and 11% respectively.
+    Customer account interest needs to be implemented.
+    Implement a solution to print the interest on the basis of principal, time, & rate given by the user.
+    Design an appropriate class and method to implement this.
+
+'''
+
+class Interest:
+    def __init__(self, principal : float, time : float, acc_type : str):
+        self.__principal = principal
+        self.__time = time
+        self.__type = acc_type
+        #set the interest rate based on account type
+        if acc_type.lower() == "saving":
+            self.__rate = 11.0
+        elif acc_type.lower() == "fixed":
+            self.__rate = 4.0
+        else:
+            raise ValueError("Invalid account type")
+
+    @property
+    def get_principal(self):
+        return self.__principal
+    
+    # @get_principal.setter
+    # def set_principal(self, amount):
+    #     if not isinstance(amount,float):
+    #         raise ValueError("The amount must be of float type")
+    #     elif not amount:
+    #         raise ValueError("The amount must be present")
+    #     else:
+    #         self.__principal = amount
+    
+    @property
+    def get_time(self):
+        return self.__time
+    
+    # @get_time.setter
+    # def set_time(self, time):
+    #     if not isinstance(time, float):
+    #         raise ValueError("The time entered must of float type")
+    #     elif not time:
+    #         raise ValueError("The time must be entered")
+    #     else:
+    #         self.__time = time
+    
+    @property
+    def get_rate(self):
+        return self.__rate
+    
+    # @get_rate.setter
+    # def set_rate(self, rate):
+    #     if not isinstance(rate, float):
+    #         raise ValueError("The rate must be of float type")
+    #     elif not rate:
+    #         raise ValueError("The rate must be entered")
+    #     else:
+    #         self.__rate = rate
+
+    @property
+    def get_type(self):
+        return self.__type
+
+
+    def __str__(self):
+        return f"""
+                Account type: {self.__type}
+                Principal Amount: {self.__principal}
+                Interest rate: {self.__rate}
+                Duration: {self.__time}
+                """
